e19eebbe46baeca308ce75203b86e21b1d015b5e
nunodinov2018-art <nunodinov2018@gmail.com>
2025-12-11 14:51:20 +0000

    Update and rename ro-xploit.lua to ro-xpl0it.lua

--- ro-xpl0it.lua
+++ ro-xpl0it.lua
@@ -2,7 +2,7 @@
 --!optimize 2
 local NLS=require(13482937602)()
 NLS([[
-	pcall(@native function()
+	pcall(function()
 Folder = game.Players.LocalPlayer.PlayerGui
 script=Instance.new('LocalScript')
 local a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z 
